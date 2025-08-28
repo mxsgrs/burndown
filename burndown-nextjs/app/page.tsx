1bfd5cae01e9d36caec5079b2f3b07a48aef04c5
Maxime Gros <60075323+mxsgrs@users.noreply.github.com>
2025-08-28 23:31:38 +0200

    Center and max width

--- burndown-nextjs/app/page.tsx
+++ burndown-nextjs/app/page.tsx
@@ -1,5 +1,11 @@
 import { ChartAreaStackedExpand } from "./chart"
 
 export default function Home() {
-  return <ChartAreaStackedExpand/>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="w-full max-w-3xl">
+        <ChartAreaStackedExpand />
+      </div>
+    </div>
+  )
 }
